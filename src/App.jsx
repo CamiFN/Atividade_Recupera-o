c725eb02a2dae9b8f6d5ad9581d75af0ea34293a
CamiFN <camillefstn2005@gmail.com>
2022-07-01 09:07:32 -0400

    3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import HelloWorld from './HelloWorld'
 import Button from './Button'
-
 import './App.css'
 
 function App() {
+
+  const palavras = [
+  "linux", "docker", "server", "rails", "douane", "my-developments","command-line", 
+  "ruby", "c-2", "linux-on-mac", "maintenance", "security", "gnome", "kubernetes",
+  "mes-developpements", "ubuntu", "python", "vcs", "capybara", "contribution", "git",
+  "apt","bazaar", "chef", "cucumber", "debian", "game", "howto", "packaging", "testing",
+  "boost", "debug","devise", "elixir", "gtk", "homebrew", "information", "ssh", "tuxtremsplit", "xpath"
+  ]
+  const lista = palavras.map((c)=> 
+  <p className='caixa'>{c}</p>
+  )
   
   return (
-    <div> 
-      <Button/> 
+  <div className='Elementos'> 
+      <Button/>
+  <p></p>
+  <div className='palavras'>
+        {lista}
+        <p></p>
+    </div>
+
   </div>
         )
   }
